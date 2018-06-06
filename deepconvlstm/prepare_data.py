464c5cd145dcc3e21ae76b53baf28b3fa2a6788d
b40153 <b40153@localhost.localdomain>
2018-06-06 17:09:14 -0300

    Greatly improving prepare_data, by coupling time_steps of lstm with frequency rate, and adding windows consistency check.

--- deepconvlstm/prepare_data.py
+++ deepconvlstm/prepare_data.py
@@ -16,6 +16,10 @@ K.set_learning_phase(1)
 np.random.seed(1)
 BATCH_SIZE = 16
 LEARN_RATE = 0.001
+# Size, in seconds, of an entire movement (5s movement, + 3s rest)
+ENTIRE_MOV_DURATION = 8
+# Size, em ms, of the time window where each classification will take place
+OBS_WINDOW = 200
 DATASETS_DICT = {
     "dataset_1": {
         "dataset_path": "../datasets/db1/",
@@ -24,7 +28,7 @@ DATASETS_DICT = {
         "import_func": nh.import_db1,
         "dataset_info": nh.db1_info(),
         "history_path": "../history/db1/",
-        "ts_number": 400,
+        "window_factor": 0.1,
         "train_split": 3
     },
     "dataset_2": {
@@ -34,7 +38,7 @@ DATASETS_DICT = {
         "import_func": nh.import_db2,
         "dataset_info": nh.db2_info(),
         "history_path": "../history/db2_batchsize"+str(BATCH_SIZE)+"/",
-        "ts_number": 1000,
+        "window_factor": 2,
         "train_split": 2
     },
     "dataset_3": {
@@ -44,7 +48,7 @@ DATASETS_DICT = {
         "import_func": nh.import_db3,
         "dataset_info": nh.db3_info(),
         "history_path": "../history/db3/",
-        "ts_number": 1000,
+        "window_factor": 2,
         "train_split": 2
     },
 }
@@ -154,7 +158,7 @@ def get_deepconvlstm(input_shape, subject, class_number, dataset, monitor='val_a
     return (model, callbacks_list)
 
 
-def prepare_data(subject, timesteps_number, inc_len, train_split, dataset,
+def prepare_data(subject, timesteps_number, train_split, dataset,
                  subsamp_rate=1, select_classes=None):
     """Get data from dataset and assemble as 4D input tensor for a keras model.
 
@@ -162,8 +166,6 @@ def prepare_data(subject, timesteps_number, inc_len, train_split, dataset,
         subject (:obj:`int`): 0 for gathering data from all subjects,
             or any other integer in the subjects range for a single subject
         timesteps_number (:obj:`int`): number of time steps for each sequence
-        inc_len (:obj:`int`): size in ms of the moving window increment
-            (only multiples of 10) Ex: 10, 20, 30, ...
         train_split (:obj:`int`): 1-10, proportion of test-train split, based on
             number of repetitions (10) Ex: 3 indicates 30% of test samples
         dataset (:obj:`str`): dataset number: dataset_1, dataset_2 or dataset_3
@@ -179,6 +181,8 @@ def prepare_data(subject, timesteps_number, inc_len, train_split, dataset,
         y_test (:obj:`4d-array`): a 4-D array to be used as categorical output test samples
 
     """
+    inc_len = int((OBS_WINDOW*DATASETS_DICT[dataset]["window_factor"])/subsamp_rate)
+    init_fs = int((DATASETS_DICT[dataset]["dataset_info"]["fs"]*(OBS_WINDOW/1000))/subsamp_rate)
     reps = DATASETS_DICT[dataset]["dataset_info"]['rep_labels']
     # Get EMG, repetition and movement data, don't cap maximum length of rest
     subject_dict = DATASETS_DICT[dataset]["import_func"](DATASETS_DICT[dataset]["dataset_path"],
@@ -193,11 +197,24 @@ def prepare_data(subject, timesteps_number, inc_len, train_split, dataset,
     # for use with Keras
     # y_all: movement label, length: number of windows
     # r_all: repetition label, length: number of windows
-    x_all, y_all, r_all = nh.get_windows(reps, timesteps_number, int(inc_len/10),
+    x_all, y_all, r_all = nh.get_windows(reps, timesteps_number, inc_len,
                                          emg_data, subject_dict['move'][::subsamp_rate],
                                          subject_dict['rep'][::subsamp_rate],
                                          which_moves=select_classes,
                                          dtype=np.float16)
+    x_sq = np.squeeze(x_all)
+    win_1 = x_sq[0, :, 1]
+    win_2 = x_sq[1, :, 1]
+    win_3 = x_sq[2, :, 1]
+    # Checking consistency of 3 first windows...
+    first_check = win_1[2*init_fs-1] == win_2[init_fs-1]
+    sec_check = win_2[init_fs] == win_3[0]
+    if not (first_check and sec_check):
+        expected_samples = subject_dict['emg'][::subsamp_rate].shape[0]/(inc_len)
+        print ('Expected samples: {}'.format(expected_samples))
+        print ('Proportion expected/got: {}'.format(expected_samples/x_all.shape[0]))
+        raise Exception("Windows are not coherent!")
+    # Window data: x_all data is 4D tensor [observation, time_step, channel, 1]
     train_idx = nh.get_idxs(r_all, train_reps[0, :])
     test_idx = nh.get_idxs(r_all, test_reps[0, :])
     y_train = y_all[train_idx]
@@ -214,12 +231,26 @@ def prepare_data(subject, timesteps_number, inc_len, train_split, dataset,
     return (x_train, y_train, x_test, y_test)
 
 
-def run_trainning(dataset, inc_len, epochs, subsamp_rate, moves=None):
+def run_training(dataset, timeback_reach, epochs, subsamp_rate, moves=None):
+    """Train the deepconvlstm model, given a dataset.
+
+    Arguments:
+        dataset (:obj:`str`): dataset number: dataset_1, dataset_2 or dataset_3
+        timeback_reach (:obj:`int`): time in seconds on how far back the lstm
+            cells will look into the past
+        timesteps_number (:obj:`int`): number of time steps for each sequence
+        epochs (:obj:`int`): number of epochs for training
+        subsamp_rate (:obj:`int`, *default* =1): subsample rate. Ex: 2 indicates
+            that all dataset will be subsampled to a half
+        moves (:obj:`list`,optional, *default* =None): If given, the classes
+            used for the classifier will be only these ones; else, all classes
+
+    """
     ds_dict = DATASETS_DICT[dataset]
     Path(ds_dict['weights_path']).mkdir(parents=True, exist_ok=True)
     Path(ds_dict['log_dir']).mkdir(parents=True, exist_ok=True)
     Path(ds_dict['history_path']).mkdir(parents=True, exist_ok=True)
-    timestep_num = ds_dict["ts_number"]
+    timestep_num = int((ds_dict["dataset_info"]["fs"]*timeback_reach)/subsamp_rate)
     train_split = ds_dict["train_split"]
     try:
         classes = ds_dict["dataset_info"]["nb_moves"] if not moves else len(moves)
@@ -228,13 +259,13 @@ def run_trainning(dataset, inc_len, epochs, subsamp_rate, moves=None):
     w_folder = ds_dict["weights_path"]
     logger.info('Starting training process...')
     logger.info('Epochs:{}, timesteps_number:{}, step_len:{} ms, batch size:{} samples'.
-                format(epochs, timestep_num, inc_len, BATCH_SIZE))
-    for subject_number in range(1, ds_dict["dataset_info"]["nb_subjects"]+1):
+                format(epochs, timestep_num, OBS_WINDOW, BATCH_SIZE))
+    for subject_number in range(10, ds_dict["dataset_info"]["nb_subjects"]+1):
         if not classes:
             classes = ds_dict["dataset_info"]['subjects'][subject_number-1]["nb_moves"]
         print ('Subject {}, number of classes: {}'.format(subject_number, classes))
         logger.info('Running training for subject {}...'.format(subject_number))
-        sub_data = prepare_data(subject_number, timestep_num, inc_len, train_split,
+        sub_data = prepare_data(subject_number, timestep_num, train_split,
                                 dataset, subsamp_rate, moves)
         input_shape = sub_data[0].shape
         model, callbacks_list = get_deepconvlstm(input_shape[1:], subject_number,
@@ -267,11 +298,14 @@ def run_trainning(dataset, inc_len, epochs, subsamp_rate, moves=None):
 
 
 if __name__ == "__main__":
-    inc_len = 200
     epochs = 150
-    subsamp_rate = 20
+    # Rate of subsampling (reduces memory and cpu usage but accuracy too)
+    subsamp_rate = 16
+    reach_number = 1.25
+    timeback_reach = ENTIRE_MOV_DURATION*reach_number
+    run_training('dataset_3', timeback_reach, epochs, subsamp_rate)
     for dataset in DATASETS_DICT.keys():
         if dataset == 'dataset_1':
             subsamp_rate = 1
-        run_trainning(dataset, inc_len, epochs, subsamp_rate)
+        run_training(dataset, timeback_reach, epochs, subsamp_rate)
 
